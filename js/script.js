17b414e226834d8e9cc72882d3071ce5147bafd1
matteoiorio <iorio436@outlook.it>
2021-10-25 16:42:54 +0200

    buzzfizz update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 /*
-CREATE AN ALGORITHM THAT DIVIDE DIFFERENT VARIABLES CICLE RESULTS
+CREATE AN ALGORITHM THAT DIVIDE DIFFERENT VARIABLES CYCLE RESULTS
 BASED ON DIVISION BY 3, 5 AND BY 3 AND 5
 */
+const divSquare = document.getElementById('block_container');
 
-//create variables
+for (let i = 1; i <=100; i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    divSquare.append(square);
+
+    if(i % 15 === 0) {
+        square.classList.add('fizzBuzz');
+        square.append("FizzBuzz");
+    }else if(i % 5 === 0) {
+        square.classList.add('divBuzz');
+        square.append("Buzz")
+    }else if (i % 3 === 0) {
+        square.classList.add('divFizz');
+        square.append("Fizz");
+    }else {
+        square.append(i);
+    }
+}
